130b34f41130759d963dcccee1943aef95d28134
Chirag Goyal <chiraggoyal.vnm@gmail.com>
2025-08-31 22:15:57 +0530

    Update q3.py

--- q3.py
+++ q3.py
@@ -1,7 +1,5 @@
-n = int(input())
-for i in range(1, n+1):
-    for j in range(n-i):
-        print(" ", end="")
-    for j in range(i):
-        print("*", end="")
+n= 6
+for i in range(n):
+    for j in range(i+1,n):
+        print("*", end=" ")
     print()
