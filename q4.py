83590b4e42360b57c6870bf364539da8acf79714
Chirag Goyal <chiraggoyal.vnm@gmail.com>
2025-09-01 00:33:04 +0530

    Update q4.py

--- q4.py
+++ q4.py
@@ -1,5 +1,8 @@
-n = int(input())
-for i in range(1, n+1):
-    for j in range(1, i+1):
-        print(j, end="")
+n =6
+for i in range(n):
+    for j in range(i+1):
+        if (j%2==1):
+            print(j,end=" ")
+        else:
+            print("*" ,end=" ")
     print()
