5182ec370d4afe23da2225c93d27b9d92c20d487
Chirag Goyal <chiraggoyal.vnm@gmail.com>
2025-09-01 00:47:10 +0530

    Update q5.py

--- q5.py
+++ q5.py
@@ -1,7 +1,8 @@
-n = int(input())
+n =5
 for i in range(n):
-    for j in range(i):
-        print(" ", end="")
-    for j in range(n-i):
-        print("*", end="")
+    for j in range (n):
+        if (j ==0 or j==4):
+            print("*",end=" ")
+        else:
+            print("_" ,end=" ")
     print()
