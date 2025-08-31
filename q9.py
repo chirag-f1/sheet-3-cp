09e5a825d3ceaf805670e061f069c70be6e441cc
Chirag Goyal <chiraggoyal.vnm@gmail.com>
2025-09-01 01:14:26 +0530

    Update q9.py

--- q9.py
+++ q9.py
@@ -1,9 +1,9 @@
-n = int(input())
-for i in range(1, n+1):
-    for j in range(1, i+1):
-        print("*", end="")
-    print()
-for i in range(n-1, 0, -1):
-    for j in range(i):
-        print("*", end="")
+n = 5
+for i in range(n):
+    for j in range(n-i):
+        print("*",end="")
+    for j in range(2*i):
+        print("_",end="")
+    for j in range(n-i):
+         print("*",end="")
     print()
