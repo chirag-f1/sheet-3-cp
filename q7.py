12fafcc5f9ac48fbe5303843faac0a5888f8130c
Chirag Goyal <chiraggoyal.vnm@gmail.com>
2025-09-01 01:02:17 +0530

    Update q7.py

--- q7.py
+++ q7.py
@@ -1,9 +1,7 @@
-n = int(input())
-for i in range(1, n+1):
-    for j in range(i):
-        print("*", end=" ")
-    print()
-for i in range(n-1, 0, -1):
-    for j in range(i):
-        print("*", end=" ")
+n =5
+for i in range(n):
+    for j in range(n-i-1):
+        print("_",end="")
+    for j in range(i+1):
+        print("*",end=" ")
     print()
