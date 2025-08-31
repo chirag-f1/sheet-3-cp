b6b194bce766aca99a173add8d761f9f5d28a7e4
Chirag Goyal <chiraggoyal.vnm@gmail.com>
2025-09-01 01:05:51 +0530

    Update q8.py

--- q8.py
+++ q8.py
@@ -1,13 +1,8 @@
-n = int(input())
+n=5
 for i in range(n):
     for j in range(i):
-        print(" ", end="")
+        print("_",end="")
     for j in range(n-i):
-        print("*", end="")
+        print("*",end=" ")
     print()
-for i in range(2, n+1):
-    for j in range(n-i):
-        print(" ", end="")
-    for j in range(i):
-        print("*", end="")
-    print()
+    
