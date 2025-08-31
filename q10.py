008fa526ff9250c96bfe86ce3ae8544111d0057b
Chirag Goyal <chiraggoyal.vnm@gmail.com>
2025-09-01 01:27:21 +0530

    Update q10.py

--- q10.py
+++ q10.py
@@ -1,8 +1,10 @@
-n = int(input())
-for i in range(1, n+1):
-    for j in range(1, n+1):
-        if j == 1 or j == n or i == 1 or i == n:
-            print("*", end="")
-        else:
-            print(" ", end="")
+n =6
+
+for i in range(n):
+    for j in range(i):
+        print("*",end="")
+    for j in range(2*(n-i)-2):
+        print(" ",end="")
+    for j in range(i):
+        print("*",end="")
     print()
