88e2295db3737fc0abdbdd70a0d0d666f6a2e0d7
Chirag Goyal <chiraggoyal.vnm@gmail.com>
2025-09-01 00:53:39 +0530

    Update q6.py

--- q6.py
+++ q6.py
@@ -1,7 +1,9 @@
-n = int(input())
+n = 6
 for i in range(n):
-    for j in range(n-i-1):
-        print(" ", end="")
-    for j in range(i+1):
-        print("*", end="")
+    for j in range(1):
+        print("*" ,end=" ")
+    for j in range(i+1,n):
+        print("_",end=" ")
+    for j in range(1):
+        print("*",end=" ")
     print()
